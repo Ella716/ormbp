738e288b834a1e74a6dca9b86c0a1f0a3eb9bb0c
Ella716 <60780983+Ella716@users.noreply.github.com>
2024-01-15 23:24:14 -0300

    Delete src directory

--- src/main/java/sr/unasat/bp24/hibernate/factory/ProductFactory.java
+++
@@ -1,9 +0,0 @@
-package sr.unasat.bp24.hibernate.factory;
-
-import sr.unasat.bp24.hibernate.entity.Product;
-
-public class ProductFactory {
-    public static Product maakProduct(String naam, double prijs) {
-        return new Product(naam, prijs);
-    }
-}
